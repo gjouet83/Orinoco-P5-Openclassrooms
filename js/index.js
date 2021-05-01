7997e9debd5671cec754164d0d08306c300b5cf9
gjouet83 <76901532+gjouet83@users.noreply.github.com>
2021-05-01 14:56:59 +0200

    amélioration du header

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const tableau = document.getElementById("productsList")
+
+fetch ("http://localhost:3000/api/cameras")
+let test = document.getElementById("5be1ed3f1c9d44000030b061")
+console.log(test)
