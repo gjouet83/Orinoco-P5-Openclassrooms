ba6a348ea218b26e571539b60fe0eba8702f384d
gjouet83 <76901532+gjouet83@users.noreply.github.com>
2021-05-01 18:38:11 +0200

    changement de présentation du header

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const tableau = document.getElementById("productsList")
-
-fetch ("http://localhost:3000/api/cameras")
-let test = document.getElementById("5be1ed3f1c9d44000030b061")
-console.log(test)
