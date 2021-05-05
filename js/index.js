ea2ae14a13dd8b99b768ba29d8a360b3fad2c716
gjouet83 <76901532+gjouet83@users.noreply.github.com>
2021-05-05 08:46:35 +0200

    séparation de la fonction creatDiv, améliorations mineures de la mise en page desktop

--- js/index.js
+++ js/index.js
@@ -10,17 +10,23 @@ const getPromise = () => {
         return cameras;
     })
 }
-const creatDiv = () => {
-    let newDiv = document.createElement("div");
+
+let newDiv = 0;
+const createDiv = () => {
+    newDiv = document.createElement("div");
     document
         .getElementById("productList")
         .appendChild(newDiv);
     newDiv.classList.add("productList__element");
+}
+const createName = () => {
     let newName = document.createElement("h3");
     newDiv.appendChild(newName);
     newName.classList.add("productList__element__name");
     let name = document.createTextNode(`${camera.name}`);
     newName.appendChild(name);
+}
+const createPicture = () => {
     let newPicture = document.createElement("div");
     newDiv.appendChild(newPicture);
     newPicture.classList.add("productList__element__picture");
@@ -30,6 +36,8 @@ const creatDiv = () => {
     newImg.src = `${camera.imageUrl}`;
     newImg.alt = `appareil photo ${camera.name}`;
     newFigure.appendChild(newImg);
+}
+const createPrice = () => {
     let newPrice = document.createElement("div");
     newDiv.appendChild(newPrice);
     newPrice.classList.add("productList__element__price");
@@ -38,6 +46,8 @@ const creatDiv = () => {
     newValue.classList.add("productList__element__price__value");
     let value = document.createTextNode(`${camera.price}€`);
     newValue.appendChild(value);
+}
+const createBtn = () => {
     let newButton = document.createElement("a");
     let viewProduct = document.createTextNode("Voir le produit");
     newButton.appendChild(viewProduct);
@@ -45,12 +55,18 @@ const creatDiv = () => {
     newDiv.appendChild(newButton);
     newButton.classList.add("productList__element__viewProduct");
     newButton.classList.add("btn");
-
+}
+const createThumbnail = () => {
+    createDiv();
+    createName();
+    createPicture();
+    createPrice();
+    createBtn();
 }
 const getCameras = async () => {
     const cameras = await getPromise();
     for (camera of cameras) {
-        creatDiv();
+        createThumbnail();
     }
 }
 
